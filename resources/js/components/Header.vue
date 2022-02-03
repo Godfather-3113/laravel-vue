a586209613f3b2b1e2524ca4389342d63ff4776c
godfather <media.art.press2@gmail.com >
2022-02-03 14:32:40 +0500

    add_style

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-center">
+
+            <ul class="uk-navbar-nav">
+
+            </ul>
+
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Header",
+
+    data() {
+        return {
+            links: [
+                {
+                    title: "Главная",
+                    href: "/"
+                },
+                {
+                    title: "Блог",
+                    href: "/blog"
+                },
+                {
+                    title: "Создать пост",
+                    href: "/create"
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
